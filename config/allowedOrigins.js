4e597be82afbf40746523b62cd69575d3b73d369
AndreyTolochko <45091849+AndreyTolochko@users.noreply.github.com>
2024-02-08 18:17:36 +0000

    modified allowedOrigins added https://serverdm.onrender.com

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,8 @@
 const allowedOrigins=[
     'https://seeyourdestiny.onrender.com/',
     'http://seeyourdestiny.onrender.com',
+    'https://serverdm.onrender.com',
+    ' http://serverdm.onrender.com',
     'www.seeyourdestiny.onrender.com',
 ]
 
